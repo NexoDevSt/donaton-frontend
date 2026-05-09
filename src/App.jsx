77fa917e5acc7f55dddd8d742192609f930650df
Matias1402 <matias18torres18@gmail.com>
2026-05-08 22:15:47 -0400

    feat(frontend): implementadas  páginas de Login y Register y configurar rutas en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { Toaster } from 'react-hot-toast'; // Importante para las alertas
 import Layout from './components/layout/Layout';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Toaster position="top-right" /> 
+        
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<LandingPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
